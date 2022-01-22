91125789010078c06b9120aafb0871d9bb1e0aae
ivansu995 <61602291+ivansu995@users.noreply.github.com>
2022-01-22 17:39:22 +0100

    Finalna verzija v1.2

--- logika/dodajZadatak.php
+++ logika/dodajZadatak.php
@@ -19,7 +19,7 @@ function reArrayFiles(&$file_post)
     $file_count = count($file_post['name']);
     $file_keys = array_keys($file_post);
 
-    for ($i=0; $i<$file_count; $i++) {
+    for ($i = 0; $i < $file_count; $i++) {
         foreach ($file_keys as $key) {
             $file_ary[$i][$key] = $file_post[$key][$i];
         }
